49051ee0f145d71e6adf197c41e8ac4637a9f6a7
Rene Peinthor <peinthor@gmail.com>
2020-05-04 07:45:28 +0200

    config: virter.toml shouldn't be executable

--- cmd/config.go
+++ cmd/config.go
@@ -144,7 +144,7 @@ func writeDefaultConfig(path string) error {
 		return fmt.Errorf("failed to render template: %w", err)
 	}
 
-	err = ioutil.WriteFile(path, defaultConfig.Bytes(), 0700)
+	err = ioutil.WriteFile(path, defaultConfig.Bytes(), 0600)
 	if err != nil {
 		return fmt.Errorf("failed to write default config file: %w", err)
 	}
