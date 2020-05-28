d0a18c71f4f4424291c6ce06d3a9668efb28bc97
Christoph Böhmwalder <christoph.boehmwalder@linbit.com>
2020-05-28 10:42:13 +0200

    registry: fetch shipped image registry from GitHub
    Re-introduce the images.toml file, but this time we take a differnet
    approach: instead of using the assets directly (or – more recently – hard
    coding the default image registry value in the Go code), we fetch the latest
    version of the image registry from GitHub pages if it does not exist locally.

--- cmd/registry.go
+++ cmd/registry.go
@@ -2,7 +2,8 @@ package cmd
 
 import (
 	"fmt"
-	"io/ioutil"
+	"io"
+	"net/http"
 	"os"
 	"path/filepath"
 
@@ -11,31 +12,7 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-const defaultRegistry = `[ubuntu-focal]
-url = "https://cloud-images.ubuntu.com/focal/current/focal-server-cloudimg-amd64.img"
-
-[ubuntu-bionic]
-url = "https://cloud-images.ubuntu.com/bionic/current/bionic-server-cloudimg-amd64.img"
-
-[ubuntu-xenial]
-url = "https://cloud-images.ubuntu.com/xenial/current/xenial-server-cloudimg-amd64-disk1.img"
-
-[debian-10]
-url = "https://cloud.debian.org/images/cloud/buster/daily/20200119-143/debian-10-generic-amd64-daily-20200119-143.qcow2"
-
-[debian-9]
-# There is no publicly available "generic" image, but the OpenStack one works
-url = "https://cdimage.debian.org/cdimage/openstack/current-9/debian-9-openstack-amd64.qcow2"
-
-[centos-8]
-url = "https://cloud.centos.org/centos/8/x86_64/images/CentOS-8-GenericCloud-8.1.1911-20200113.3.x86_64.qcow2"
-
-[centos-7]
-url = "https://cloud.centos.org/centos/7/images/CentOS-7-x86_64-GenericCloud.qcow2"
-
-[centos-6]
-url = "https://cloud.centos.org/centos/6/images/CentOS-6-x86_64-GenericCloud.qcow2"
-`
+const upstreamRegistryURL = "https://linbit.github.io/virter/images.toml"
 
 func defaultRegistryPath() string {
 	dataHome := os.Getenv("XDG_DATA_HOME")
@@ -53,18 +30,36 @@ func userRegistryFile() string {
 	return filepath.Join(configPath(), "images.toml")
 }
 
-func writeDefaultRegistry(path string) error {
+func fetchShippedRegistry(path string) error {
 	dir := filepath.Dir(path)
 	err := os.MkdirAll(dir, 0700)
 	if err != nil {
-		return fmt.Errorf("failed to create default registry directory ('%v'): %w",
+		return fmt.Errorf("failed to create shipped registry directory ('%v'): %w",
 			dir, err)
 	}
 
-	err = ioutil.WriteFile(path, []byte(defaultRegistry), 0700)
+	log.Debugf("Fetching shipped registry from '%s'", upstreamRegistryURL)
+
+	resp, err := http.Get(upstreamRegistryURL)
 	if err != nil {
-		return fmt.Errorf("failed to write default registry file: %w", err)
+		return err
 	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("HTTP error %s", resp.Status)
+	}
+
+	file, err := os.Create(path)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	_, err = io.Copy(file, resp.Body)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
@@ -72,9 +67,9 @@ func loadRegistry() *registry.ImageRegistry {
 	registryPath := filepath.Join(defaultRegistryPath(), "images.toml")
 	if _, err := os.Stat(registryPath); os.IsNotExist(err) {
 		log.Infof("Builtin image registry does not exist, writing to %v", registryPath)
-		err := writeDefaultRegistry(registryPath)
+		err := fetchShippedRegistry(registryPath)
 		if err != nil {
-			log.Warnf("Failed to write builtin image registry file: %v", err)
+			log.Warnf("Failed to fetch builtin image registry file: %v", err)
 			log.Warnf("Proceeding with only user-defined images")
 		}
 	}
