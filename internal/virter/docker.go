LINBIT/virter

commit 9539aaf4a09cba90a40c9e82f1deca61a5ab2982

podman integration via containerapi package

internal/virter/docker.go
@@ -10,30 +10,11 @@ import (
 	"sync"
 	"time"
 
+	"github.com/LINBIT/containerapi"
+
 	log "github.com/sirupsen/logrus"
 	"golang.org/x/crypto/ssh/terminal"
-
-	"github.com/docker/docker/api/types"
-	"github.com/docker/docker/api/types/container"
-	"github.com/docker/docker/api/types/network"
-	"github.com/docker/docker/pkg/stdcopy"
 )
-
-// DockerClient contains the required docker methods.
-type DockerClient interface {
-	ContainerCreate(ctx context.Context, config *container.Config, hostConfig *container.HostConfig, networkingConfig *network.NetworkingConfig, containerName string) (container.ContainerCreateCreatedBody, error)
-	ContainerStart(ctx context.Context, containerID string, options types.ContainerStartOptions) error
-	ContainerStop(ctx context.Context, containerID string, timeout *time.Duration) error
-	ContainerWait(ctx context.Context, containerID string, condition container.WaitCondition) (<-chan container.ContainerWaitOKBody, <-chan error)
-	ContainerLogs(ctx context.Context, container string, options types.ContainerLogsOptions) (io.ReadCloser, error)
-}
-
-// DockerContainerConfig contains the configuration for a to be started container
-type DockerContainerConfig struct {
-	ContainerName string   // the name of the container
-	ImageName     string   // the name of the container image
-	Env           []string // the environment (variables) passed to the container
-}
 
 const (
 	colorDefault = colorReset
@@ -41,47 +22,39 @@ const (
 	colorReset   = "\u001b[0m"
 )
 
-func dockerRun(ctx context.Context, docker DockerClient, dockerContainerConfig DockerContainerConfig, vmIPs []string, sshPrivateKey []byte) error {
+func containerRun(ctx context.Context, containerProvider containerapi.ContainerProvider, containerCfg *containerapi.ContainerConfig, vmIPs []string, sshPrivateKey []byte) error {
 	// This is roughly equivalent to
 	// docker run --rm --network=host -e TARGETS=$vmIPs -e SSH_PRIVATE_KEY="$sshPrivateKey" $dockerImageName
 
-	targetEnv := fmt.Sprintf("TARGETS=%s", strings.Join(vmIPs, ","))
-	sshPrivateKeyEnv := fmt.Sprintf("SSH_PRIVATE_KEY=%s", sshPrivateKey)
+	containerCfg.SetEnv("TARGETS", strings.Join(vmIPs, ","))
+	containerCfg.SetEnv("SSH_PRIVATE_KEY", string(sshPrivateKey))
 
-	resp, err := docker.ContainerCreate(
+	resp, err := containerProvider.Create(
 		ctx,
-		&container.Config{
-			Image: dockerContainerConfig.ImageName,
-			Env:   append(dockerContainerConfig.Env, targetEnv, sshPrivateKeyEnv),
-		},
-		&container.HostConfig{
-			NetworkMode: "host",
-			AutoRemove:  true,
-		},
-		nil,
-		dockerContainerConfig.ContainerName)
+		containerCfg,
+	)
 	if err != nil {
 		return fmt.Errorf("could not create container: %w", err)
 	}
 
-	statusCh, errCh := docker.ContainerWait(ctx, resp.ID, container.WaitConditionRemoved)
+	statusCh, errCh := containerProvider.Wait(ctx, resp)
 
-	err = docker.ContainerStart(ctx, resp.ID, types.ContainerStartOptions{})
+	err = containerProvider.Start(ctx, resp)
 	if err != nil {
 		return fmt.Errorf("could not start container: %w", err)
 	}
 
-	// dockerStreamLogs is ctx safe (i.e., errs out in copy if ctx cancled)
-	err = dockerStreamLogs(ctx, docker, resp.ID)
+	// streamLogs is ctx safe (i.e., errs out in copy if ctx cancled)
+	err = streamLogs(ctx, containerProvider, resp)
 	if err != nil { // something weird happened here, most likely context canceled
 		td := 200 * time.Millisecond // this horse is already dead...
-		if stopErr := docker.ContainerStop(context.Background(), resp.ID, &td); stopErr != nil {
+		if stopErr := containerProvider.Stop(context.Background(), resp, &td); stopErr != nil {
 			return fmt.Errorf("could not stop container: %v, after log streaming failed: %w", stopErr, err)
 		}
 		return err
 	}
 
-	err = dockerContainerWait(ctx, statusCh, errCh)
+	err = containerWait(ctx, statusCh, errCh)
 	if err != nil {
 		return err
 	}
@@ -89,12 +62,8 @@ func dockerRun(ctx context.Context, docker DockerClient, dockerContainerConfig D
 	return nil
 }
 
-func dockerStreamLogs(ctx context.Context, docker DockerClient, id string) error {
-	out, err := docker.ContainerLogs(
-		ctx, id,
-		types.ContainerLogsOptions{
-			ShowStdout: true, ShowStderr: true, Follow: true,
-		})
+func streamLogs(ctx context.Context, containerProvider containerapi.ContainerProvider, id string) error {
+	stdout, stderr, err := containerProvider.Logs(ctx, id)
 	if err != nil {
 		return fmt.Errorf("could not get container logs: %w", err)
 	}
@@ -102,19 +71,8 @@ func dockerStreamLogs(ctx context.Context, docker DockerClient, id string) error
 	var wg sync.WaitGroup
 	wg.Add(2)
 
-	stdoutReader, stdoutWriter := io.Pipe()
-	stderrReader, stderrWriter := io.Pipe()
-
-	go logLines(&wg, "Docker", false, stdoutReader)
-	go logLines(&wg, "Docker", true, stderrReader)
-
-	_, err = stdcopy.StdCopy(stdoutWriter, stderrWriter, out)
-	if err != nil {
-		return fmt.Errorf("error copying container output: %w", err)
-	}
-
-	stdoutWriter.Close()
-	stderrWriter.Close()
+	go logLines(&wg, "Docker", false, stdout)
+	go logLines(&wg, "Docker", true, stderr)
 
 	wg.Wait()
 	return nil
@@ -151,16 +109,15 @@ func logLines(wg *sync.WaitGroup, vm string, stderr bool, r io.Reader) {
 	}
 }
 
-func dockerContainerWait(ctx context.Context, statusCh <-chan container.ContainerWaitOKBody, errCh <-chan error) error {
+func containerWait(ctx context.Context, statusCh <-chan int64, errCh <-chan error) error {
 	select {
+	case <- ctx.Done():
+		return fmt.Errorf("timeout waiting for container: %v", ctx.Err())
 	case err := <-errCh:
 		return fmt.Errorf("error waiting for container: %w", err)
 	case status := <-statusCh:
-		if status.Error != nil {
-			return fmt.Errorf("error from container '%s' (exit code %d)", status.Error.Message, status.StatusCode)
-		}
-		if status.StatusCode != 0 {
-			return fmt.Errorf("container returned non-zero exit code %d", status.StatusCode)
+		if status != 0 {
+			return fmt.Errorf("container returned non-zero exit code %d", status)
 		}
 	}
 	return nil

cmd/config.go
@@ -47,12 +47,12 @@ ssh_ping_period = "{{ get "time.ssh_ping_period" }}"
 # Default value: "{{ get "time.shutdown_timeout" }}"
 shutdown_timeout = "{{ get "time.shutdown_timeout" }}"
 
-# docker_timeout is how long virter will wait for a Docker provisioning step to
+# container_timeout is how long virter will wait for a container provisioning step to
 # complete.
-# If a Docker provisioning step exceeds this timeout, it will be aborted and an
+# If a container provisioning step exceeds this timeout, it will be aborted and an
 # error will be produced.
-# Default value: "{{ get "time.docker_timeout" }}"
-docker_timeout = "{{ get "time.docker_timeout" }}"
+# Default value: "{{ get "time.container_timeout" }}"
+container_timeout = "{{ get "time.container_timeout" }}"
 
 [auth]
 # virter_public_key_path is where virter should place its generated public key.
@@ -78,6 +78,10 @@ virter_private_key_path = "{{ get "auth.virter_private_key_path" }}"
 # user's authorized keys inside the VM.
 # Default value: "{{ get "auth.user_public_key" }}"
 user_public_key = "{{ get "auth.user_public_key" }}"
+
+[container]
+# provider is the container engine used. Can be either podman or docker.
+provider = "{{ get "container.provider" }}"
 `
 
 // initConfig reads in config file and ENV variables if set.
@@ -87,7 +91,8 @@ func initConfig() {
 	viper.SetDefault("time.ssh_ping_count", 60)
 	viper.SetDefault("time.ssh_ping_period", time.Second)
 	viper.SetDefault("time.shutdown_timeout", 20*time.Second)
-	viper.SetDefault("time.docker_timeout", 30*time.Minute)
+	viper.SetDefault("time.container_timeout", 30*time.Minute)
+	viper.SetDefault("container.provider", "docker")
 
 	viper.SetConfigType("toml")
 	if cfgFile != "" {

cmd/image_build.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"github.com/LINBIT/containerapi"
 	"github.com/rck/unit"
 	log "github.com/sirupsen/logrus"
 
@@ -41,7 +42,7 @@ step, and then committing the resulting volume.`,
 			baseImageName := args[0]
 			newImageName := args[1]
 
-			dctx, dcancel := dockerContext()
+			dctx, dcancel := containerContext()
 			defer dcancel()
 			ctx, cancel := onInterruptWrap(dctx)
 			defer cancel()
@@ -75,7 +76,7 @@ step, and then committing the resulting volume.`,
 
 			shutdownTimeout := viper.GetDuration("time.shutdown_timeout")
 
-			// DockerClient will be set later if needed
+			// ContainerProvider will be set later if needed
 			tools := virter.ImageBuildTools{
 				ShellClientBuilder: SSHClientBuilder{},
 				AfterNotifier:      actualtime.ActualTime{},
@@ -96,9 +97,7 @@ step, and then committing the resulting volume.`,
 				ConsolePath:     consolePath,
 			}
 
-			dockerContainerConfig := virter.DockerContainerConfig{
-				ContainerName: "virter-build-" + newImageName,
-			}
+			containerName := "virter-build-" + newImageName
 
 			provOpt := virter.ProvisionOption{
 				FilePath:  provisionFile,
@@ -109,22 +108,22 @@ step, and then committing the resulting volume.`,
 			if err != nil {
 				log.Fatal(err)
 			}
-			if provisionConfig.NeedsDocker() {
-				docker, err := dockerConnect()
+			if provisionConfig.NeedsContainers() {
+				containerProvider, err := containerapi.NewProvider(ctx, containerProvider())
 				if err != nil {
 					log.Fatal(err)
 				}
-				defer docker.Close()
-				tools.DockerClient = docker
+				defer containerProvider.Close()
+				tools.ContainerProvider = containerProvider
 			}
 
 			buildConfig := virter.ImageBuildConfig{
-				DockerContainerConfig: dockerContainerConfig,
-				SSHPrivateKeyPath:     privateKeyPath,
-				SSHPrivateKey:         privateKey,
-				ShutdownTimeout:       shutdownTimeout,
-				ProvisionConfig:       provisionConfig,
-				ResetMachineID:        resetMachineID,
+				ContainerName:     containerName,
+				SSHPrivateKeyPath: privateKeyPath,
+				SSHPrivateKey:     privateKey,
+				ShutdownTimeout:   shutdownTimeout,
+				ProvisionConfig:   provisionConfig,
+				ResetMachineID:    resetMachineID,
 			}
 
 			err = pullIfNotExists(v, vmConfig.ImageName)

cmd/vm.go
@@ -6,7 +6,6 @@ import (
 	"io/ioutil"
 	"strings"
 
-	"github.com/docker/docker/client"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 	"golang.org/x/crypto/ssh"
@@ -31,18 +30,13 @@ func vmCommand() *cobra.Command {
 	return vmCmd
 }
 
-func dockerConnect() (*client.Client, error) {
-	docker, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
-	if err != nil {
-		return nil, fmt.Errorf("could not connect to Docker %w", err)
-	}
-
-	return docker, nil
+func containerProvider() string {
+	return viper.GetString("container.provider")
 }
 
-func dockerContext() (context.Context, context.CancelFunc) {
-	dockerTimeout := viper.GetDuration("time.docker_timeout")
-	return context.WithTimeout(context.Background(), dockerTimeout)
+func containerContext() (context.Context, context.CancelFunc) {
+	containerTimeout := viper.GetDuration("time.container_timeout")
+	return context.WithTimeout(context.Background(), containerTimeout)
 }
 
 // SSHClientBuilder builds SSH shell clients

cmd/vm_cp.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"context"
+
 	"github.com/LINBIT/virter/pkg/netcopy"
 
 	log "github.com/sirupsen/logrus"

cmd/vm_exec.go
@@ -6,6 +6,7 @@ import (
 
 	log "github.com/sirupsen/logrus"
 
+	"github.com/LINBIT/containerapi"
 	"github.com/LINBIT/virter/internal/virter"
 	"github.com/LINBIT/virter/pkg/netcopy"
 
@@ -69,29 +70,25 @@ func execProvision(provOpt virter.ProvisionOption, vmNames []string) error {
 }
 
 func execDocker(v *virter.Virter, s *virter.ProvisionDockerStep, vmNames []string) error {
-	dctx, dcancel := dockerContext()
+	dctx, dcancel := containerContext()
 	defer dcancel()
 	ctx, cancel := onInterruptWrap(dctx)
 	defer cancel()
 
-	docker, err := dockerConnect()
+	containerProvider, err := containerapi.NewProvider(ctx, containerProvider())
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer docker.Close()
+	defer containerProvider.Close()
 
 	privateKey, err := loadPrivateKey()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	dockerContainerConfig := virter.DockerContainerConfig{
-		ContainerName: "virter-" + strings.Join(vmNames, "-"),
-		ImageName:     s.Image,
-		Env:           virter.EnvmapToSlice(s.Env),
-	}
+	containerCfg := containerapi.NewContainerConfig("virter-" + strings.Join(vmNames, "-"), s.Image, s.Env)
 
-	return v.VMExecDocker(ctx, docker, vmNames, dockerContainerConfig, privateKey)
+	return v.VMExecDocker(ctx, containerProvider, vmNames, containerCfg, privateKey)
 }
 
 func execShell(v *virter.Virter, s *virter.ProvisionShellStep, vmNames []string) error {

go.mod
@@ -3,55 +3,31 @@ module github.com/LINBIT/virter
 go 1.13
 
 require (
-	github.com/Azure/go-ansiterm v0.0.0-20170929234023-d6e3b3328b78 // indirect
 	github.com/BurntSushi/toml v0.3.1
+	github.com/LINBIT/containerapi v0.1.0
 	github.com/LINBIT/gosshclient v0.3.1
-	github.com/Microsoft/go-winio v0.4.14 // indirect
 	github.com/VividCortex/ewma v1.1.1 // indirect
-	github.com/containerd/containerd v1.3.3 // indirect
 	github.com/digitalocean/go-libvirt v0.0.0-20190715144809-7b622097a793
-	github.com/docker/distribution v2.7.1+incompatible // indirect
-	github.com/docker/docker v1.4.2-0.20200309214505-aa6a9891b09c
-	github.com/docker/go-connections v0.4.0 // indirect
-	github.com/docker/go-units v0.4.0 // indirect
-	github.com/gogo/protobuf v1.3.1 // indirect
-	github.com/golang/protobuf v1.3.5 // indirect
-	github.com/gorilla/mux v1.7.4 // indirect
 	github.com/hashicorp/go-multierror v1.1.0
 	github.com/helm/helm v2.16.6+incompatible
 	github.com/kdomanski/iso9660 v0.0.0-20200428203439-00eb28aa394d
-	github.com/konsorten/go-windows-terminal-sequences v1.0.2 // indirect
-	github.com/kr/pretty v0.1.0
+	github.com/kr/pretty v0.2.0
 	github.com/libvirt/libvirt-go-xml v6.1.0+incompatible
 	github.com/mattn/go-isatty v0.0.12 // indirect
 	github.com/mitchellh/go-homedir v1.1.0
-	github.com/mitchellh/mapstructure v1.1.2
-	github.com/morikuni/aec v1.0.0 // indirect
-	github.com/opencontainers/go-digest v1.0.0-rc1 // indirect
-	github.com/opencontainers/image-spec v1.0.1 // indirect
-	github.com/pelletier/go-toml v1.6.0 // indirect
-	github.com/pkg/errors v0.9.1 // indirect
+	github.com/mitchellh/mapstructure v1.3.3
 	github.com/rck/unit v0.0.3
 	github.com/satori/go.uuid v1.2.0
 	github.com/sethvargo/go-signalcontext v0.1.0
-	github.com/sirupsen/logrus v1.5.0
-	github.com/spf13/afero v1.2.2 // indirect
-	github.com/spf13/cast v1.3.1 // indirect
+	github.com/sirupsen/logrus v1.6.0
 	github.com/spf13/cobra v0.0.5
-	github.com/spf13/jwalterweatherman v1.1.0 // indirect
-	github.com/spf13/pflag v1.0.5 // indirect
-	github.com/spf13/viper v1.6.2
-	github.com/stretchr/testify v1.5.1
+	github.com/spf13/viper v1.7.0
+	github.com/stretchr/testify v1.6.1
 	github.com/vbauerster/mpb v3.4.0+incompatible
 	github.com/vektra/mockery v1.1.2
-	golang.org/x/crypto v0.0.0-20200429183012-4b2356b1ed79
-	golang.org/x/net v0.0.0-20200324143707-d3edc9973b7e // indirect
-	golang.org/x/sync v0.0.0-20190911185100-cd5d95a43a6e
-	golang.org/x/sys v0.0.0-20200323222414-85ca7c5b95cd
-	golang.org/x/text v0.3.2 // indirect
-	google.golang.org/genproto v0.0.0-20200325114520-5b2d0af7952b // indirect
-	google.golang.org/grpc v1.28.0 // indirect
-	gopkg.in/ini.v1 v1.52.0 // indirect
-	gopkg.in/yaml.v2 v2.2.8 // indirect
-	gotest.tools v2.2.0+incompatible // indirect
+	golang.org/x/crypto v0.0.0-20200709230013-948cd5f35899
+	golang.org/x/sync v0.0.0-20200625203802-6e8e738ad208
 )
+
+// to enable libvirt authentication via policykit
+replace github.com/digitalocean/go-libvirt v0.0.0-20190715144809-7b622097a793 => github.com/wanzenbug/go-libvirt v0.0.0-20200901120615-7281076f1c61

commit 69f5a226f95dd45db80f9f7aca4356e0c517ccf6

provisioning: stop container and exit with error on context cancellation
This fixes 2 issues that occurred when interrupting a container
provisioning step with a signal.

Firstly, the container was not removed.

Secondly, virter sometimes exited with exit code 0. A non-zero exit code
is expected in this situation.

These issues were regressions introduced in:
9539aaf4a09c podman integration via containerapi package

internal/virter/docker.go
@@ -29,7 +29,7 @@ func containerRun(ctx context.Context, containerProvider containerapi.ContainerP
 	containerCfg.SetEnv("TARGETS", strings.Join(vmIPs, ","))
 	containerCfg.SetEnv("SSH_PRIVATE_KEY", string(sshPrivateKey))
 
-	resp, err := containerProvider.Create(
+	containerID, err := containerProvider.Create(
 		ctx,
 		containerCfg,
 	)
@@ -37,18 +37,16 @@ func containerRun(ctx context.Context, containerProvider containerapi.ContainerP
 		return fmt.Errorf("could not create container: %w", err)
 	}
 
-	statusCh, errCh := containerProvider.Wait(ctx, resp)
+	statusCh, errCh := containerProvider.Wait(ctx, containerID)
 
-	err = containerProvider.Start(ctx, resp)
+	err = containerProvider.Start(ctx, containerID)
 	if err != nil {
 		return fmt.Errorf("could not start container: %w", err)
 	}
 
-	// streamLogs is ctx safe (i.e., errs out in copy if ctx cancled)
-	err = streamLogs(ctx, containerProvider, resp)
-	if err != nil { // something weird happened here, most likely context canceled
-		td := 200 * time.Millisecond // this horse is already dead...
-		if stopErr := containerProvider.Stop(context.Background(), resp, &td); stopErr != nil {
+	err = streamLogs(ctx, containerProvider, containerID)
+	if err != nil {
+		if stopErr := containerStop(containerProvider, containerID); stopErr != nil {
 			return fmt.Errorf("could not stop container: %v, after log streaming failed: %w", stopErr, err)
 		}
 		return err
@@ -56,9 +54,22 @@ func containerRun(ctx context.Context, containerProvider containerapi.ContainerP
 
 	err = containerWait(ctx, statusCh, errCh)
 	if err != nil {
+		if stopErr := containerStop(containerProvider, containerID); stopErr != nil {
+			return fmt.Errorf("could not stop container: %v, after wait finished: %w", stopErr, err)
+		}
 		return err
 	}
 
+	return nil
+}
+
+func containerStop(containerProvider containerapi.ContainerProvider, containerID string) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+	td := 200 * time.Millisecond // this horse is already dead...
+	if stopErr := containerProvider.Stop(ctx, containerID, &td); stopErr != nil {
+		return stopErr
+	}
 	return nil
 }
 
@@ -111,8 +122,8 @@ func logLines(wg *sync.WaitGroup, vm string, stderr bool, r io.Reader) {
 
 func containerWait(ctx context.Context, statusCh <-chan int64, errCh <-chan error) error {
 	select {
-	case <- ctx.Done():
-		return fmt.Errorf("timeout waiting for container: %v", ctx.Err())
+	case <-ctx.Done():
+		// end wait
 	case err := <-errCh:
 		return fmt.Errorf("error waiting for container: %w", err)
 	case status := <-statusCh:
@@ -120,5 +131,13 @@ func containerWait(ctx context.Context, statusCh <-chan int64, errCh <-chan erro
 			return fmt.Errorf("container returned non-zero exit code %d", status)
 		}
 	}
+
+	// select is not biased amongst the cases. This means that there may be
+	// a context error even when we terminated the select due to a zero
+	// status on statusCh.
+	if ctx.Err() != nil {
+		return fmt.Errorf("timeout waiting for container: %v", ctx.Err())
+	}
+
 	return nil
 }