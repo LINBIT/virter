05ad608f58002c69b60a14872b88ae4ac21da961
Joel Colledge <joel.colledge@linbit.com>
2021-02-22 10:22:35 +0100

    examples: simplify docker example
    Since
    7d40669ae6cc generate ssh host key for VMs
    it is no longer necessary to write the private key file or turn off host
    key checking.

--- examples/hello-world/entry.sh
+++ examples/hello-world/entry.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
-mkdir /root/.ssh
-echo "$SSH_PRIVATE_KEY" > /root/.ssh/id_rsa
-chmod 600 /root/.ssh/id_rsa
 for h in $(echo "$TARGETS" | tr ',' '\n'); do
-	/usr/bin/ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null $h "echo hello world > /file"
+	ssh $h "echo hello world > /file"
 done
