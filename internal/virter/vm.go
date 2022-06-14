LINBIT/virter

commit 777a4f883f02d3446fcf29e7a185ce064c680c77

Removed -u option from vm ssh and vm exec, store user name in VM's meta data
This patch stores the user name for ssh sessions given at the
vm run or image build command for later use in a vm ssh or vm exec
command.

This was also requested by Moritz.

internal/virter/vm.go
@@ -111,6 +111,7 @@ func (v *Virter) VMRun(vmConfig VMConfig) error {
 
 	meta := &VMMeta{
 		HostKey: hostkey.PublicKey(),
+		SSHUserName: vmConfig.SSHUserName,
 	}
 
 	vmXML, err := v.vmXML(v.provisionStoragePool.Name, vmConfig, mac, meta)
@@ -170,7 +171,7 @@ func diskVolumeName(vmName, diskName string) string {
 }
 
 // WaitVmReady repeatedly tries to connect to a VM and checks if it's ready to be used.
-func (v *Virter) WaitVmReady(ctx context.Context, shellClientBuilder ShellClientBuilder, vmName string, readyConfig VmReadyConfig, remoteUser string) error {
+func (v *Virter) WaitVmReady(ctx context.Context, shellClientBuilder ShellClientBuilder, vmName string, readyConfig VmReadyConfig) error {
 	ips, err := v.getIPs([]string{vmName})
 	if err != nil {
 		return err
@@ -187,6 +188,11 @@ func (v *Virter) WaitVmReady(ctx context.Context, shellClientBuilder ShellClient
 	}
 
 	hostkeyCheck, supportedAlgos := knownHosts.AsHostKeyConfig()
+
+	remoteUser,err := v.getSSHUserName(vmName)
+	if err != nil {
+		return fmt.Errorf("Failed to get SSH user name for VM %v : %w", vmName, err)
+	}
 
 	sshConfig := ssh.ClientConfig{
 		Auth:              v.sshkeys.Auth(),
@@ -499,6 +505,14 @@ func (v *Virter) getKnownHostsFor(vmNames ...string) (sshkeys.KnownHosts, error)
 	return knownHosts, nil
 }
 
+func (v *Virter) getSSHUserName(vmName string) (string, error) {
+	meta, err := v.getMetaForVM(vmName)
+	if err != nil {
+		return "", err
+	}
+	return meta.SSHUserName, nil
+}
+
 // VMExecDocker runs a docker container against some VMs.
 func (v *Virter) VMExecDocker(ctx context.Context, containerProvider containerapi.ContainerProvider, vmNames []string, containerCfg *containerapi.ContainerConfig, copyStep *ProvisionDockerCopyStep) error {
 	ips, err := v.getIPs(vmNames)
@@ -542,7 +556,7 @@ func (v *Virter) VMExecDocker(ctx context.Context, containerProvider containerap
 }
 
 // VMSSHSession runs an interactive shell session in a VM
-func (v *Virter) VMSSHSession(ctx context.Context, vmName string, remoteUser string) error {
+func (v *Virter) VMSSHSession(ctx context.Context, vmName string) error {
 	ips, err := v.getIPs([]string{vmName})
 	if err != nil {
 		return err
@@ -557,6 +571,11 @@ func (v *Virter) VMSSHSession(ctx context.Context, vmName string, remoteUser str
 	}
 
 	hostkeyCheck, supportedAlgos := knownHosts.AsHostKeyConfig()
+
+	remoteUser,err := v.getSSHUserName(vmName)
+	if err != nil {
+		return fmt.Errorf("Failed to get SSH user name for VM %v : %w", vmName, err)
+	}
 
 	sshConfig := ssh.ClientConfig{
 		Auth:              v.sshkeys.Auth(),
@@ -576,7 +595,7 @@ func (v *Virter) VMSSHSession(ctx context.Context, vmName string, remoteUser str
 }
 
 // VMExecShell runs a simple shell command against some VMs.
-func (v *Virter) VMExecShell(ctx context.Context, vmNames []string, shellStep *ProvisionShellStep, remoteUser string) error {
+func (v *Virter) VMExecShell(ctx context.Context, vmNames []string, shellStep *ProvisionShellStep) error {
 	ips, err := v.getIPs(vmNames)
 	if err != nil {
 		return err
@@ -591,7 +610,6 @@ func (v *Virter) VMExecShell(ctx context.Context, vmNames []string, shellStep *P
 
 	sshConfig := ssh.ClientConfig{
 		Auth:              v.sshkeys.Auth(),
-		User:              remoteUser,
 		HostKeyCallback:   hostkeyCheck,
 		HostKeyAlgorithms: supportedAlgos,
 	}
@@ -600,6 +618,13 @@ func (v *Virter) VMExecShell(ctx context.Context, vmNames []string, shellStep *P
 	for i, ip := range ips {
 		ip := ip
 		vmName := vmNames[i]
+
+		remoteUser,err := v.getSSHUserName(vmName)
+		if err != nil {
+			return err
+		}
+		sshConfig.User = remoteUser
+
 		log.Println("Provisioning via SSH:", shellStep.Script, "in", ip)
 		g.Go(func() error {
 			return runSSHCommand(ctx, &sshConfig, vmName, net.JoinHostPort(ip, "22"), shellStep.Script, EnvmapToSlice(shellStep.Env))

cmd/image_build.go
@@ -189,6 +189,7 @@ func imageBuildCommand() *cobra.Command {
 				ConsolePath:        consolePath,
 				DiskCache:          viper.GetString("libvirt.disk_cache"),
 				Mounts:             mounts,
+				SSHUserName:        user,
 			}
 
 			containerName := "virter-build-" + newImageName
@@ -212,7 +213,7 @@ func imageBuildCommand() *cobra.Command {
 
 			p = mpb.NewWithContext(ctx, DefaultContainerOpt())
 
-			err = v.ImageBuild(ctx, tools, vmConfig, getReadyConfig(), buildConfig, user, virter.WithProgress(DefaultProgressFormat(p)))
+			err = v.ImageBuild(ctx, tools, vmConfig, getReadyConfig(), buildConfig, virter.WithProgress(DefaultProgressFormat(p)))
 			if err != nil {
 				log.Fatalf("Failed to build image: %v", err)
 			}

cmd/vm_exec.go
@@ -19,7 +19,6 @@ import (
 func vmExecCommand() *cobra.Command {
 	var provisionFile string
 	var provisionOverrides []string
-	var user string
 
 	var containerPullPolicy pullpolicy.PullPolicy
 
@@ -38,7 +37,7 @@ func vmExecCommand() *cobra.Command {
 				DefaultPullPolicy:  getDefaultContainerPullPolicy(),
 				OverridePullPolicy: containerPullPolicy,
 			}
-			if err := execProvision(ctx, provOpt, args, user); err != nil {
+			if err := execProvision(ctx, provOpt, args); err != nil {
 				log.Fatal(err)
 			}
 		},
@@ -48,12 +47,11 @@ func vmExecCommand() *cobra.Command {
 	execCmd.Flags().StringVarP(&provisionFile, "provision", "p", "", "name of toml file containing provisioning steps")
 	execCmd.Flags().StringArrayVarP(&provisionOverrides, "set", "s", []string{}, "set/override provisioning steps")
 	execCmd.Flags().VarP(&containerPullPolicy, "container-pull-policy", "", fmt.Sprintf("Whether or not to pull container images used durign provisioning. Overrides the `pull` value of every provision step. Valid values: [%s, %s, %s]", pullpolicy.Always, pullpolicy.IfNotExist, pullpolicy.Never))
-        execCmd.Flags().StringVarP(&user, "user", "u", "root", "Remote user for ssh session")
 
 	return execCmd
 }
 
-func execProvision(ctx context.Context, provOpt virter.ProvisionOption, vmNames []string, user string) error {
+func execProvision(ctx context.Context, provOpt virter.ProvisionOption, vmNames []string) error {
 	pc, err := virter.NewProvisionConfig(provOpt)
 	if err != nil {
 		return err
@@ -70,7 +68,7 @@ func execProvision(ctx context.Context, provOpt virter.ProvisionOption, vmNames 
 				return err
 			}
 		} else if s.Shell != nil {
-			if err := v.VMExecShell(ctx, vmNames, s.Shell, user); err != nil {
+			if err := v.VMExecShell(ctx, vmNames, s.Shell); err != nil {
 				return err
 			}
 		} else if s.Rsync != nil {

cmd/vm_run.go
@@ -234,6 +234,7 @@ func vmRunCommand() *cobra.Command {
 						VNCEnabled:         vncEnabled,
 						VNCPort:	    vncPort,
 						VNCIPv4BindAddress: vncIPv4BindAddress,
+						SSHUserName:        user,
 					}
 
 					err = v.VMRun(c)
@@ -242,7 +243,7 @@ func vmRunCommand() *cobra.Command {
 					}
 
 					if waitSSH {
-						err = v.WaitVmReady(ctx, SSHClientBuilder{}, thisVMName, getReadyConfig(), user)
+						err = v.WaitVmReady(ctx, SSHClientBuilder{}, thisVMName, getReadyConfig())
 						if err != nil {
 							return fmt.Errorf("Failed to connect to VM %d over SSH: %w", id, err)
 						}
@@ -261,7 +262,7 @@ func vmRunCommand() *cobra.Command {
 					DefaultPullPolicy:  getDefaultContainerPullPolicy(),
 					OverridePullPolicy: containerPullPolicy,
 				}
-				if err := execProvision(ctx, provOpt, vmNames, user); err != nil {
+				if err := execProvision(ctx, provOpt, vmNames); err != nil {
 					log.Fatal(err)
 				}
 			}

cmd/vm_ssh.go
@@ -8,8 +8,6 @@ import (
 )
 
 func vmSSHCommand() *cobra.Command {
-	var user string
-
 	sshCmd := &cobra.Command{
 		Use:   "ssh vm_name",
 		Short: "Run an interactive ssh shell in a VM",
@@ -22,13 +20,12 @@ func vmSSHCommand() *cobra.Command {
 			}
 			defer v.ForceDisconnect()
 
-			if err := v.VMSSHSession(context.TODO(), args[0], user); err != nil {
+			if err := v.VMSSHSession(context.TODO(), args[0]); err != nil {
 				log.Fatal(err)
 			}
 		},
 		ValidArgsFunction: suggestVmNames,
 	}
-	sshCmd.Flags().StringVarP(&user, "user", "u", "root", "Remote user for ssh session")
 
 	return sshCmd
 }

internal/virter/image.go
@@ -537,11 +537,11 @@ type ImageBuildConfig struct {
 	ResetMachineID  bool
 }
 
-func (v *Virter) imageBuildProvisionCommit(ctx context.Context, tools ImageBuildTools, vmConfig VMConfig, readyConfig VmReadyConfig, buildConfig ImageBuildConfig, user string, opts ...LayerOperationOption) error {
+func (v *Virter) imageBuildProvisionCommit(ctx context.Context, tools ImageBuildTools, vmConfig VMConfig, readyConfig VmReadyConfig, buildConfig ImageBuildConfig, opts ...LayerOperationOption) error {
 	vmNames := []string{vmConfig.Name}
 	var err error
 
-	err = v.WaitVmReady(ctx, tools.ShellClientBuilder, vmConfig.Name, readyConfig, user)
+	err = v.WaitVmReady(ctx, tools.ShellClientBuilder, vmConfig.Name, readyConfig)
 	if err != nil {
 		return err
 	}
@@ -568,7 +568,7 @@ func (v *Virter) imageBuildProvisionCommit(ctx context.Context, tools ImageBuild
 			)
 			err = v.VMExecDocker(ctx, tools.ContainerProvider, vmNames, containerCfg, nil)
 		} else if s.Shell != nil {
-			err = v.VMExecShell(ctx, vmNames, s.Shell, user)
+			err = v.VMExecShell(ctx, vmNames, s.Shell)
 		} else if s.Rsync != nil {
 			copier := netcopy.NewRsyncNetworkCopier()
 			err = v.VMExecRsync(ctx, copier, vmNames, s.Rsync)
@@ -588,7 +588,7 @@ func (v *Virter) imageBuildProvisionCommit(ctx context.Context, tools ImageBuild
 }
 
 // ImageBuild builds an image by running a VM and provisioning it.
-func (v *Virter) ImageBuild(ctx context.Context, tools ImageBuildTools, vmConfig VMConfig, readyConfig VmReadyConfig, buildConfig ImageBuildConfig, user string, opts ...LayerOperationOption) error {
+func (v *Virter) ImageBuild(ctx context.Context, tools ImageBuildTools, vmConfig VMConfig, readyConfig VmReadyConfig, buildConfig ImageBuildConfig, opts ...LayerOperationOption) error {
 	// VMRun is responsible to call CheckVMConfig here!
 	// TODO(): currently we can not know why VM run failed, so we don't clean up in this stage,
 	//         it could have been an existing VM, we don't want to delete it.
@@ -598,7 +598,7 @@ func (v *Virter) ImageBuild(ctx context.Context, tools ImageBuildTools, vmConfig
 	}
 
 	// from here on it is safe to rm the VM if something fails
-	err = v.imageBuildProvisionCommit(ctx, tools, vmConfig, readyConfig, buildConfig, user, opts...)
+	err = v.imageBuildProvisionCommit(ctx, tools, vmConfig, readyConfig, buildConfig, opts...)
 	if err != nil {
 		log.Warn("could not build image, deleting VM")
 		if rmErr := v.VMRm(vmConfig.Name, !vmConfig.StaticDHCP, true); rmErr != nil {

internal/virter/virter.go
@@ -150,11 +150,13 @@ type VMConfig struct {
 	VNCEnabled	   bool
 	VNCPort            int
 	VNCIPv4BindAddress string
+	SSHUserName        string
 }
 
 // VMMeta is additional metadata stored with each VM
 type VMMeta struct {
-	HostKey string `xml:"hostkey"`
+	HostKey     string `xml:"hostkey"`
+	SSHUserName string `xml:"ssh-user-name"`
 }
 
 // VmReadyConfig contains the configuration for waiting for a VM to be ready.

internal/virter/vm_test.go
@@ -96,7 +96,7 @@ func TestWaitVmReady(t *testing.T) {
 
 	v := virter.New(l, poolName, networkName, newMockKeystore())
 
-	err := v.WaitVmReady(context.Background(), MockShellClientBuilder{shell}, vmName, readyConfig, "root")
+	err := v.WaitVmReady(context.Background(), MockShellClientBuilder{shell}, vmName, readyConfig)
 	assert.NoError(t, err)
 
 	shell.AssertExpectations(t)

commit c1b5cfc392e2d20ac1b629fa43eb568249cb837d

Default ssh user to root if not found in meta data
For examples VMs created with an earlier version of virter.

This should also fix a race condition introduced with
777a4f883f0 by creating the ssh config within the loop
in VMExecShell().

internal/virter/vm.go
@@ -189,10 +189,7 @@ func (v *Virter) WaitVmReady(ctx context.Context, shellClientBuilder ShellClient
 
 	hostkeyCheck, supportedAlgos := knownHosts.AsHostKeyConfig()
 
-	remoteUser,err := v.getSSHUserName(vmName)
-	if err != nil {
-		return fmt.Errorf("Failed to get SSH user name for VM %v : %w", vmName, err)
-	}
+	remoteUser := v.getSSHUserName(vmName)
 
 	sshConfig := ssh.ClientConfig{
 		Auth:              v.sshkeys.Auth(),
@@ -505,12 +502,12 @@ func (v *Virter) getKnownHostsFor(vmNames ...string) (sshkeys.KnownHosts, error)
 	return knownHosts, nil
 }
 
-func (v *Virter) getSSHUserName(vmName string) (string, error) {
+func (v *Virter) getSSHUserName(vmName string) string {
 	meta, err := v.getMetaForVM(vmName)
 	if err != nil {
-		return "", err
+		return "root"
 	}
-	return meta.SSHUserName, nil
+	return meta.SSHUserName
 }
 
 // VMExecDocker runs a docker container against some VMs.
@@ -572,10 +569,7 @@ func (v *Virter) VMSSHSession(ctx context.Context, vmName string) error {
 
 	hostkeyCheck, supportedAlgos := knownHosts.AsHostKeyConfig()
 
-	remoteUser,err := v.getSSHUserName(vmName)
-	if err != nil {
-		return fmt.Errorf("Failed to get SSH user name for VM %v : %w", vmName, err)
-	}
+	remoteUser := v.getSSHUserName(vmName)
 
 	sshConfig := ssh.ClientConfig{
 		Auth:              v.sshkeys.Auth(),
@@ -608,22 +602,19 @@ func (v *Virter) VMExecShell(ctx context.Context, vmNames []string, shellStep *P
 
 	hostkeyCheck, supportedAlgos := knownHosts.AsHostKeyConfig()
 
-	sshConfig := ssh.ClientConfig{
-		Auth:              v.sshkeys.Auth(),
-		HostKeyCallback:   hostkeyCheck,
-		HostKeyAlgorithms: supportedAlgos,
-	}
-
 	var g errgroup.Group
 	for i, ip := range ips {
 		ip := ip
 		vmName := vmNames[i]
 
-		remoteUser,err := v.getSSHUserName(vmName)
-		if err != nil {
-			return err
+		remoteUser := v.getSSHUserName(vmName)
+		sshConfig := ssh.ClientConfig{
+			Auth:              v.sshkeys.Auth(),
+			User:		   remoteUser,
+			HostKeyCallback:   hostkeyCheck,
+			HostKeyAlgorithms: supportedAlgos,
 		}
-		sshConfig.User = remoteUser
+
 
 		log.Println("Provisioning via SSH:", shellStep.Script, "in", ip)
 		g.Go(func() error {