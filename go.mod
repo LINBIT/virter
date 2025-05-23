a9dae3d4e33ca02ed50aca506a751ab7f91ab675
Moritz Wanzenböck <moritz.wanzenboeck@linbit.com>
2025-05-23 10:20:33 +0200

    bump go version to 1.24
    Signed-off-by: Moritz Wanzenböck <moritz.wanzenboeck@linbit.com>

--- go.mod
+++ go.mod
@@ -1,7 +1,8 @@
 module github.com/LINBIT/virter
 
-go 1.23.0
-toolchain go1.24.1
+go 1.24.0
+
+toolchain go1.24.3
 
 require (
 	github.com/BurntSushi/toml v1.5.0
--- internal/virter/virter_test.go
+++ internal/virter/virter_test.go
@@ -7,10 +7,10 @@ import (
 	"github.com/LINBIT/virter/internal/virter/mocks"
 )
 
-//go:generate go run github.com/vektra/mockery/v2@v2.40.3 --name=ShellClient
-//go:generate go run github.com/vektra/mockery/v2@v2.40.3 --name=AfterNotifier
-//go:generate go run github.com/vektra/mockery/v2@v2.40.3 --name=NetworkCopier --dir=../../pkg/netcopy
-//go:generate go run github.com/vektra/mockery/v2@v2.40.3 --name=KeyStore --dir=../../pkg/sshkeys
+//go:generate go run github.com/vektra/mockery/v2@v2.53.4 --name=ShellClient
+//go:generate go run github.com/vektra/mockery/v2@v2.53.4 --name=AfterNotifier
+//go:generate go run github.com/vektra/mockery/v2@v2.53.4 --name=NetworkCopier --dir=../../pkg/netcopy
+//go:generate go run github.com/vektra/mockery/v2@v2.53.4 --name=KeyStore --dir=../../pkg/sshkeys
 
 const poolName = "some-pool"
 const networkName = "some-network"
